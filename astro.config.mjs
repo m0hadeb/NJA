7b5b07ab6d01d0cab7148ebaaa82610e4079941c
m0hadeb <m0hadeb@users.noreply.github.com>
2026-08-02 20:28:59 +0300

    Add Chinese medicine landing page design

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,3 +1,13 @@
 import { defineConfig } from 'astro/config';
 
-export default defineConfig({});
+import tailwindcss from '@tailwindcss/vite';
+
+import react from '@astrojs/react';
+
+export default defineConfig({
+  vite: {
+    plugins: [tailwindcss()]
+  },
+
+  integrations: [react()]
+});
